fa0acaedede0ee3a496aeafb0cd78b83a60b5a66
Braedon Vickers <braedon@movio.co>
2014-10-22 20:56:07 +1300

    Make queue and gpm host:port configurable

--- src/main/scala/sh/echo/QueueService.scala
+++ src/main/scala/sh/echo/QueueService.scala
@@ -7,22 +7,62 @@ import spray.http._
 import spray.http.MediaTypes._
 import spray.httpx.SprayJsonSupport._
 import spray.json._
+import JsonProtocol._
 
 object QueueService {
-  implicit val arf = Actors.system
-  import arf.dispatcher
 
-  val host = "fg.lan"
-  val port = 3000
-
-  object RegisterUser extends DefaultJsonProtocol {
+  object RegisterUser {
     implicit def jf = jsonFormat1(RegisterUser.apply)
   }
   case class RegisterUser(
-    nick: String
-  )
+    nick: String)
 
   case class RegistrationFailed(nick: String) extends Exception(s"Could not register '$nick'.")
+
+  object User {
+    implicit def jf = jsonFormat2(User.apply)
+  }
+  case class User(nick: String, waitingSince: Option[Long])
+
+  object Song {
+    implicit def jf = jsonFormat1(Song.apply)
+  }
+  case class Song(data: String)
+
+  case class QueueRequestRejected(userId: String, songId: String)
+    extends Exception(s"Could not queue '$songId' for '$userId'.")
+
+  object Queue {
+    implicit def jf = jsonFormat1(Queue.apply)
+    case class Named(userId: String, songId: String)
+  }
+  case class Queue(data: List[Map[String, String]]) {
+    def named: List[Queue.Named] = (data map { tuple ⇒
+      for {
+        userId ← tuple.keys.headOption
+        songId ← tuple.values.headOption
+      } yield Queue.Named(userId, songId)
+    }).flatten
+
+    def tupled: List[(String, String)] = (data map { tuple ⇒
+      for {
+        userId ← tuple.keys.headOption
+        songId ← tuple.values.headOption
+      } yield (userId, songId)
+    }).flatten
+  }
+
+  object NullableSong {
+    implicit def jf = jsonFormat1(NullableSong.apply)
+  }
+  case class NullableSong(data: Option[String])
+
+}
+class QueueService(host: String, port: Int) {
+  implicit val arf = Actors.system
+  import arf.dispatcher
+  import QueueService._
+
   def register(nick: String): Future[Unit] = {
     val pipeline: HttpRequest ⇒ Future[StatusCode] = (
       sendReceive
@@ -34,14 +74,6 @@ object QueueService {
     }
   }
 
-  object User extends DefaultJsonProtocol {
-    implicit def jf = jsonFormat2(User.apply)
-  }
-  case class User(
-    nick: String,
-    waitingSince: Option[Long]
-  )
-
   def listUsers(): Future[Map[String, User]] = {
     import DefaultJsonProtocol._
     val pipeline: HttpRequest ⇒ Future[Map[String, User]] = (
@@ -51,14 +83,6 @@ object QueueService {
     pipeline(Get(s"http://$host:$port/users"))
   }
 
-  object Song extends DefaultJsonProtocol {
-    implicit def jf = jsonFormat1(Song.apply)
-  }
-  case class Song(
-    data: String
-  )
-
-  case class QueueRequestRejected(userId: String, songId: String) extends Exception(s"Could not queue '$songId' for '$userId'.")
   def queue(userId: String, songId: String): Future[Unit] = {
     val pipeline: HttpRequest ⇒ Future[StatusCode] = (
       sendReceive
@@ -79,31 +103,6 @@ object QueueService {
     pipeline(Get(s"http://$host:$port/users/$userId/queue"))
   }
 
-  object Queue extends DefaultJsonProtocol {
-    implicit def jf = jsonFormat1(Queue.apply)
-    case class Named(
-      userId: String,
-      songId: String
-    )
-  }
-  case class Queue(
-    data: List[Map[String, String]]
-  ) {
-    def named: List[Queue.Named] = (data map { tuple ⇒
-      for {
-        userId ← tuple.keys.headOption
-        songId ← tuple.values.headOption
-      } yield Queue.Named(userId, songId)
-    }).flatten
-
-    def tupled: List[(String, String)] = (data map { tuple ⇒
-      for {
-        userId ← tuple.keys.headOption
-        songId ← tuple.values.headOption
-      } yield (userId, songId)
-    }).flatten
-  }
-
   def showGlobalQueue(): Future[Queue] = {
     val pipeline: HttpRequest ⇒ Future[Queue] = (
       sendReceive
@@ -111,13 +110,6 @@ object QueueService {
     )
     pipeline(Get(s"http://$host:$port/queue"))
   }
-
-  object NullableSong extends DefaultJsonProtocol with NullOptions {
-    implicit def jf = jsonFormat1(NullableSong.apply)
-  }
-  case class NullableSong(
-    data: Option[String]
-  )
 
   def lastPopped(): Future[Option[String]] = {
     val pipeline: HttpRequest ⇒ Future[NullableSong] = (
